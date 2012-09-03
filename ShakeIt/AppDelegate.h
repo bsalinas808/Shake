5689034f123972458917854b3f02d349c7ee8cd6
B Salinas <brhythmic@gmail.com>
2012-09-03 16:29:26 -0700

    Initial Commit

---
+++ ShakeIt/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  ShakeIt
+//
+//  Created by Brian Salinas on 9/3/12.
+//  Copyright (c) 2012 Bit Rhythmic Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ ShakeIt/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ShakeIt
+//
+//  Created by Brian Salinas on 9/3/12.
+//  Copyright (c) 2012 Bit Rhythmic Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
